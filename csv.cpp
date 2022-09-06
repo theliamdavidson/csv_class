bdf7907fdddc21d13abe0baab2535a4e1bbb2881
theliamdavidson <misterliamd@gmail.com>
2022-09-06 14:13:44 -0700

    increasing scope slightly

---
+++ csv.cpp
@@ -0,0 +1,8 @@
+#include "csv_read.h"
+#include "csv_write.h"
+
+using namespace std;
+
+int main(){
+
+}
---
+++ csv_write.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
