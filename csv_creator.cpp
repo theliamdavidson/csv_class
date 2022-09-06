512edeefe18c49606c835208ad8792b896035cab
theliamdavidson <misterliamd@gmail.com>
2022-09-06 14:10:02 -0700

    first commit

---
+++ csv_creator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+vector<string> create = {"Right subclavian", "Right Brachial", "Right Proximal Radial",
+                        "Right Proximal Ulnar", "Right Distal Radial","Right Distal Ulnar",
+                        "Right 1st", "Right 2nd", "Right 3rd", "Right 4th", "Left subclavian",
+                        "Left Brachial", "Left Proximal Radial", "Left Proximal Ulnar",
+                        "Left Distal radial", "Left Distal Ulnar", "Left 1st",
+                        ",Left 2nd", "Left 3rd", "Left 4th", "Right u Sciatic",
+                        "Right M Sciatic", "Right L sciatic", "Right Proximal Pero",
+                        "Right Inter Pero", "Right Low Pero", "Right Proximal tib",
+                        "Right inter tib", "Right low tib", "Right Calc", "Right Med arch",
+                        "Right Lat arch", "Right MT cutanesou", "Right 1ST", "Right 2ND",
+                        "Right 3RD", "Right 4th", "Left u Sciatic", "Left M Sciatic",
+                        "Left L sciatic", "Left Proximal Pero", "Left Inter Pero",
+                        "Left Low Pero", "Left Proximal tib", "Left inter tib",
+                        "Left low tib", "Left Calc", "Left Med arch", "Left Lat arch",
+                        "Left MT cutanesou", "Left 1ST", "Left 2ND", "Left 3RD",
+                        "Left 4th", "Right Interm ulnar","Right Interm Radial",
+                        "Left Interm Ulnar", "Left Interm Radial", "Left EI",
+                        "Lower Aorta", "Upper Aorta", "Right EI", "Intermim Aorta",
+                        "Intermed LEI", "Intermed REI", "Lower LEI", "Lower REI"};
