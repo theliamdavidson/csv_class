57be4eefaa46c37ac451aece269dbe4a113f40e7
theliamdavidson <misterliamd@gmail.com>
2022-09-07 08:11:20 -0700

    fixing spelling errors with the submitted list

--- csv_write.h
+++ csv_write.h
@@ -14,16 +14,16 @@ class Write{
                             "Right 1st", "Right 2nd", "Right 3rd", "Right 4th", "Left subclavian",
                             "Left Brachial", "Left Proximal Radial", "Left Proximal Ulnar",
                             "Left Distal radial", "Left Distal Ulnar", "Left 1st",
-                            ",Left 2nd", "Left 3rd", "Left 4th", "Right u Sciatic",
+                            "Left 2nd", "Left 3rd", "Left 4th", "Right u Sciatic",
                             "Right M Sciatic", "Right L sciatic", "Right Proximal Pero",
                             "Right Inter Pero", "Right Low Pero", "Right Proximal tib",
                             "Right inter tib", "Right low tib", "Right Calc", "Right Med arch",
-                            "Right Lat arch", "Right MT cutanesou", "Right 1ST", "Right 2ND",
+                            "Right Lat arch", "Right MT cutaneous", "Right 1ST", "Right 2ND",
                             "Right 3RD", "Right 4th", "Left u Sciatic", "Left M Sciatic",
                             "Left L sciatic", "Left Proximal Pero", "Left Inter Pero",
                             "Left Low Pero", "Left Proximal tib", "Left inter tib",
                             "Left low tib", "Left Calc", "Left Med arch", "Left Lat arch",
-                            "Left MT cutanesou", "Left 1ST", "Left 2ND", "Left 3RD",
+                            "Left MT cutaneous", "Left 1ST", "Left 2ND", "Left 3RD",
                             "Left 4th", "Right Interm ulnar","Right Interm Radial",
                             "Left Interm Ulnar", "Left Interm Radial", "Left EI",
                             "Lower Aorta", "Upper Aorta", "Right EI", "Intermim Aorta",
@@ -31,19 +31,20 @@ class Write{
     string newFilename;
     int temp;
     string int2str;
-    public:
-        void populate_file(){
-            fstream fout;
-            newFilename.append(".csv");
-            fout.open(newFilename, ios::out | ios::app);
-            // Anterograde pulsatility index, Anterograde volume flow, Retrograde volume flow, Retrograde pulsatility index
+    
+    void populate_file(){
+        fstream fout;
+        newFilename.append(".csv");
+        fout.open(newFilename, ios::out | ios::app);
+        // Anterograde pulsatility index, Anterograde volume flow, Retrograde volume flow, Retrograde pulsatility index
 
-            for(int i = 0; i < 67; i++){
-                temp = rand() % 10000000+ 10000000;
-                int2str = to_string(temp);
-                fout << create[i] << ',' << int2str[0] << int2str[1] <<',' << int2str[2] << int2str[3] << ',' << int2str[4] << int2str[5] <<',' << int2str[6] << int2str[7] << "\n";
-            }
+        for(int i = 0; i < 67; i++){
+            temp = rand() % 10000000+ 10000000;
+            int2str = to_string(temp);
+            fout << create[i] << ',' << int2str[0] << int2str[1] <<',' << int2str[2] << int2str[3] << ',' << int2str[4] << int2str[5] <<',' << int2str[6] << int2str[7] << "\n";
         }
+    }
+    public:
         Write(){
             cout << "Enter the name of the file you want to create(no extension): ";
             cin >> newFilename;
